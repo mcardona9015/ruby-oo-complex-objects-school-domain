385ae364dcefffd4b59ff8fc0aee0e6b5386d8ff
mcardona9015 <mcardona9015@gmail.com>
2020-12-08 21:09:19 -0500

    commit tree 240adbbb0dba76282c72232caa61545b851a5720

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+require 'pry'
+class School
+
+    attr_accessor :roster
+ 
+
+    def initialize(school_name)
+        @name= school_name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << student_name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_roster = {}
+        @roster.each do |grade, students|
+            sorted_roster[grade] = students.sort
+        end
+        sorted_roster
+    end
+end
+
+#binding.pry
+#{7 => ["Blake Johnson", "Jack Bauer"], 9 => ["Bart Simpson", "Homer Simpson"], 10 => ["Avi Flombaum", "Jeff Baird"]}
